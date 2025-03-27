117feaebdc371a8c2e47d552a388cd4515ab8e60
Mminjae <alswo2833@naver.com>
2025-03-28 01:39:14 +0900

    js 수정

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-const tabItem = document.querySelectorAll('.tab__item');
-const tabContent = document.querySelectorAll('.tab__content');
+const tabItems = document.querySelectorAll('.tab__item');
+const tabContents = document.querySelectorAll('.tab__content');
 
-tabItem.forEach((item, index) => {
-  item.addEventListener('click', (e) => {
-    e.preventDefault();
-    tabContent.forEach((content) => {
-      content.classList.remove('active');
-    });
-    tabItem.forEach((content) => {
-      content.classList.remove('active');
-    });
-    tabItem[index].classList.add('active');
-    tabContent[index].classList.add('active');
+// 각 탭 아이템에 클릭 이벤트를 추가
+for (let i = 0; i < tabItems.length; i++) {
+  tabItems[i].addEventListener('click', function (event) {
+    // 클릭했을 때 기본 동작 막기
+    event.preventDefault();
+
+    // 모든 탭 아이템과 탭 콘텐츠에서 active클래스를 제거
+    for (let j = 0; j < tabItems.length; j++) {
+      tabItems[j].classList.remove('active');
+      tabContents[j].classList.remove('active');
+    }
+
+    // 해당하는 콘텐츠에 active클래스를 추가
+    tabItems[i].classList.add('active');
+    tabContents[i].classList.add('active');
   });
-});
+}
